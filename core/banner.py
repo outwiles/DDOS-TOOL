88523b9e5136650401b46d582d2290d48afb3b51
outwiles <aayushhubkl99@gmail.com>
2026-09-13 12:43:39 +0530

    Add banner function to display DDOS information

---
+++ core/banner.py
@@ -0,0 +1,5 @@
+def rn():
+    try:
+        from cfonts import render;o=render("DDOS",colors=["white","blue"],align="left",background="block");print(o);print("GitHub / Telegram: @outwiles | Mail: outwiles@proton.me");print()
+    except ImportError:
+        print("DDOS");print("GitHub / Telegram: @outwiles | Mail: outwiles@proton.me");print()
